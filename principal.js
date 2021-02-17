daf3e278790e02ddad200928a0b1d51aa97e4b60
Christian Lopes Viana <christlv@trt3.jus.br>
2021-02-17 11:12:01 -0300

    Inclusão das informações da página principal

--- principal.js
+++ principal.js
@@ -21,32 +21,11 @@ function buscaDadosStatus() {
 }
 
 function limpaECarregaTabela(dadosStatus) {	
-	var qtd = dadosStatus.length;
-	var ciclo;	
-	$("#tabela-status").html("");
-	for (var numLinha = 0; numLinha < qtd; numLinha++) {
-		ciclo = refeicoes.Ciclos[numLinha];				
-		$("#tabela-status").append(criaLinhaCiclo(ciclo.SEQ, ciclo.NOME, ciclo.HORA, ciclo.QTD));
-	}	
+	$('#hora-ultima').text(dadosStatus.HORA_ULTIMA);
+	$('#qtd-ultima').text(dadosStatus.QTD_ULTIMA);
+	$('#hora-proxima').text(dadosStatus.HORA_PROXIMA);
+	$('#qtd-proxima').text(dadosStatus.QTD_PROXIMA);
 }
-
-
-function criaLinhaCiclo(seq, nome, hora, qtd) {	
-	var txtCiclo = "<a href='#' class='list-group-item list-group-item-action flex-column align-items-start' ";
-	txtCiclo += "data-dismiss='modal' data-toggle='modal' data-target='#modalRefeicao'";
-	txtCiclo += " data-seq-refeicao=" + seq;	
-	txtCiclo += " data-titulo=" + nome;
-	txtCiclo += " data-nome-refeicao=" + nome;
-	txtCiclo += " data-hora-refeicao=" + hora; 
-	txtCiclo += " data-qtd-refeicao=" + qtd + ">";
-	txtCiclo += '<div class="d-flex w-100 justify-content-between align-items-center">';
-	txtCiclo += `<p class="mb-1">${hora} - ${nome}</p>`;	
-	txtCiclo += `<span class="badge badge-primary badge-pill">${qtd}</span>`;
-	txtCiclo += '</div>';
-	txtCiclo += '</a>';
-	return txtCiclo;
-}
-
 	
 function dataFormatada() {
 	var d = new Date();
